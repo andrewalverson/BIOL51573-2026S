0373af352c27b077de4f9cb3fb063496423ca0c5
Andrew Alverson <aja@uark.edu>
2026-04-01 10:38:13 -0500

    all the functions work

--- files/python_scripts/fibonacci.py
+++ files/python_scripts/fibonacci.py
@@ -18,31 +18,45 @@ def get_args():
     # on the command line, so the default for 'store_true' is actually false
     parser.add_argument("-v", "--verbose", help="Print verbose output", action='store_true')
 
-    # parse the arguments
+    # parse the arguments and return in two steps
     args = parser.parse_args()
+    return args
+
+    # or, parse the arguments and return in one step
+    # return parser.parse_args()
+
+
 
 ###------ function to calculate the Fibonacci number
 def fib():
     # initialize two integers
     a,b = 0,1
 
-    for i in range(int(args.position)):
+    for i in range(int(beyonce.position)):
         a,b = b,a+b
 
     fibonacci_number = a
+    return fibonacci_number
 
 
 ###------ function to print the output
-def print_output():
-    if args.verbose:
-        print(f"The Fibonacci number for {args.position} is {fibonacci_number}.")
+def print_output(output):
+    if beyonce.verbose:
+        print(f"The Fibonacci number for {beyonce.position} is {output}.")
     else:
-        print(fibonacci_number)
+        print(fibnum)
 
 
 ###------ define the main() function
 def main():
-    
+    fibnum = fib()
+    print_output(fibnum)
+    # this print statement will not print variables that are local to fib()
+    # print(a, b, fibonacci_number)
+
+###------ calling get_args() happens out here on its own
+beyonce = get_args()
+
 
 # set the environment for this script
 # is this main (i.e., a standalone Python script), or
